a896864acd7b4388cbaca4f4a3ed0871b7fd6689
Yep <31861297+yuri-perphilev@users.noreply.github.com>
2013-12-01 16:35:00 +0400

    Added: bomb explosion animation

--- main/src/com/zeroage/squarez/GameController.java
+++ main/src/com/zeroage/squarez/GameController.java
@@ -175,7 +175,7 @@ public class GameController implements Controller
         @Override
         public void bomb(int x, int y, Set<int[]> blocksToExplode)
         {
-            addController(new BombExplosionController(GameController.this, blocksToExplode));
+            addController(new BombExplosionController(GameController.this, x, y, blocksToExplode));
         }
 
         @Override
