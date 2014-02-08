ef08ac7213e2a70bee15067fef772bd602284184
Yep <31861297+yuri-perphilev@users.noreply.github.com>
2014-02-08 08:08:04 +0400

    Fixed: cracked block should be passed together with other blocks to the callback for smooth dissolve

--- main/test/com/zeroage/squarez/model/CrackedTest.java
+++ main/test/com/zeroage/squarez/model/CrackedTest.java
@@ -2,6 +2,9 @@ package com.zeroage.squarez.model;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.Assert.assertThat;
 import static org.hamcrest.Matchers.*;
 
@@ -16,10 +19,73 @@ public class CrackedTest extends BaseTest
         board.fill(2, 6, 7, 1, BlockType.CRACKED);
         board.fill(4, 4, 3, 3, BlockType.BASIC);
 
+        MyGameCallbacks callbacks = new MyGameCallbacks();
+        board.setCallbacks(callbacks);
+
         printBoard();
         board.action();
         printBoard();
 
         assertThatBoardIsEmpty();
+
+        List<PositionedBlock> blocks = callbacks.getDissolvedBlocks();
+        assertThat("Dissolved blocks not null", blocks, notNullValue());
+        assertThat("There are 16 dissolved blocks", blocks, hasSize(25));
+    }
+
+    @Test
+    public void testCracked2() throws Exception
+    {
+        board.fill(2, 2, 7, 7, BlockType.CRACKED);
+        board.fill(4, 4, 3, 3, BlockType.BASIC);
+        board.set(3, 3, null);
+        board.set(3, 5, null);
+        board.set(3, 7, null);
+        board.set(5, 3, null);
+        board.set(5, 7, null);
+        board.set(7, 3, null);
+        board.set(7, 5, null);
+        board.set(7, 7, null);
+
+        MyGameCallbacks callbacks = new MyGameCallbacks();
+        board.setCallbacks(callbacks);
+
+        printBoard();
+        board.action();
+        printBoard();
+
+        assertThatBoardIsEmpty();
+
+        List<PositionedBlock> blocks = callbacks.getDissolvedBlocks();
+        assertThat("Dissolved blocks not null", blocks, notNullValue());
+        assertThat("There are 41 dissolved blocks", blocks, hasSize(41));
+    }
+
+    private static class MyGameCallbacks implements GameCallbacks
+    {
+        List<PositionedBlock> dissolvedBlocks;
+
+        @Override
+        public void dissolving(List<PositionedBlock> blocks)
+        {
+            this.dissolvedBlocks = new ArrayList<PositionedBlock>(blocks);
+        }
+
+        @Override
+        public BombCallback bomb(int x, int y)
+        {
+            return null;
+        }
+
+        @Override
+        public MissileCallback missile(int fromX, int fromY, int dX, int dY)
+        {
+            return null;
+        }
+
+        public List<PositionedBlock> getDissolvedBlocks()
+        {
+            return dissolvedBlocks;
+        }
     }
 }
